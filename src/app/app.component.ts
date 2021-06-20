bfd0997f1abaed72a831187f8c341fab085c8eb6
Rizwan Khot <khotrizwan@gmail.com>
2021-06-20 23:45:16 +0530

    Angular String Interpolation Example donee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-string-interpolation';
+  title = 'Angular String Interpolation Example';
+  count = 0;
+  i = 0;
+  constructor() { 
+  }
+
+  ngOnInit() {
+    console.log(this.title);
+    
+      setTimeout(()=>{                           //<<<---using ()=> syntax
+        this.countFn(this.count + 1)
+      }, 1000);
+    
+  }
+
+  countFn (count: any): any {
+    this.count = count;
+    console.log(count);
+    if(count < 10) {
+      setTimeout(()=>{                           //<<<---using ()=> syntax
+        this.countFn(this.count + 1)
+      }, 1000);
+    }
+  }
+
+  
 }
